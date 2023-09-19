2955106c16b42d0b921a03012d5b8f17de176c92
Lionel Regisseur <regisseur.lionel@gmail.com>
2023-09-19 11:04:47 +0200

    Feat: Added fixtures for Education and Technology

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,10 +2,12 @@
 
 namespace App\DataFixtures;
 
+use DateInterval;
 use Faker\Factory;
 use App\Entity\Project;
+use App\Entity\Education;
 use App\Entity\ProfessionalExperience;
-use DateInterval;
+use App\Entity\Technology;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -16,20 +18,19 @@ class AppFixtures extends Fixture
         $faker = Factory::create();
 
         $existingProject = $manager->getRepository(Project::class)->findAll();
-
         $existingProfessionalExperience = $manager->getRepository(ProfessionalExperience::class)->findAll();
+        $existingEducation = $manager->getRepository(Education::class)->findAll();
+
 
         $randNumber = rand(1, 3);
 
-        // $product = new Product();
-        // $manager->persist($product);
-
+        // Load fixtures for Project
         if (empty($existingProject)) {
             for ($i = 0; $i < 8; $i++) {
                 $word = $faker->word();
                 $project = new Project();
                 $project->setName($faker->sentence($randNumber))
-                    ->setDescription(($faker->paragraph($randNumber)))
+                    ->setDescription(($faker->realTextBetween(120, 200)))
                     ->setRanking($i + 1)
                     ->setRepositoryLink("https://github.com/LuckyFail8/$word")
                     ->setWebsiteLink("https://$word.com");
@@ -38,20 +39,44 @@ class AppFixtures extends Fixture
             }
         }
 
+        // Load fixtures for Professional Experience
         if (empty($existingProfessionalExperience)) {
             for ($i = 0; $i < 3; $i++) {
                 $dateTime = $faker->dateTime();
                 $endDate = clone $dateTime;
                 $endDate->add(new DateInterval("P{$randNumber}Y"));
+
                 $professionalExp = new ProfessionalExperience();
                 $professionalExp->setStartDate($dateTime)
                     ->setEndDate($endDate)
-                    ->setPosition($faker->words(2, true))
-                    ->setCompany(ucwords($faker->words(2, true)))
-                    ->setDescription(($faker->paragraph($randNumber)));
+                    ->setPosition($faker->jobTitle())
+                    ->setCompany($faker->company())
+                    ->setDescription(($faker->realTextBetween(120, 200)));
 
                 $manager->persist($professionalExp);
             }
+        }
+
+        // Load fixtures for Education
+        if (empty($existingEducation)) {
+            for ($i = 0; $i < 2; $i++) {
+                $education = new Education();
+                $education->setTitle($faker->jobTitle())
+                    ->setYear($faker->date('Y'))
+                    ->setDescription($faker->realTextBetween(80, 120));
+
+                $manager->persist($education);
+            }
+        }
+
+        // Load fixtures for Technology
+        $technologies = ['PHP', 'Symfony', 'MySQL', 'HTML', 'CSS', 'JavaScript', 'Git'];
+        foreach ($technologies as $key => $technologyData) {
+            $technology = new Technology();
+            $technology->setName($technologyData)
+                ->setRanking($key + 1);
+
+            $manager->persist($technology);
         }
 
         $manager->flush();
